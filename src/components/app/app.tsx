1ce37a89b15c2975b1c0c68fe7d89433be7166ef
Elmira Ilyasova <capricorn_26@mail.ru>
2022-07-08 18:40:26 +0700

    Add new-product component

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,20 +1,28 @@
 import * as React from "react";
 import { useState } from "react";
 
+import { Product } from "../../types";
+
 import NewProduct from "../new-product/new-product";
 import ProductList from "../product-list/product-list";
 import Statictics from "../statistics/statistics";
 import Discount from "../discount/discount";
 
+
 const App = (): JSX.Element => {
   const [products, setProducts] = useState(null);
   const [discount, setDiscount] = useState(0);
 
+  const addNewProduct = (product: Product) => {
+    const newProducts = products.slice();
+    newProducts.push(product);
+    setProducts(newProducts);
+  };
 
   return (
     <React.Fragment>
-      <NewProduct />
-      <ProductList />
+      <NewProduct addNewProduct={addNewProduct} />
+      <ProductList/>
       <Statictics />
       <Discount />
     </React.Fragment>
--- src/components/new-product/new-product.tsx
+++ src/components/new-product/new-product.tsx
@@ -1,8 +1,50 @@
 import * as React from "react";
+import { useState } from "react";
 
-const NewProduct = (): JSX.Element => {
+import { Product } from "../../types";
+
+interface NewProductProps {
+  addNewProduct: (product: Product) => void,
+};
+
+const NewProduct = ({ addNewProduct }: NewProductProps): JSX.Element => {
+
+  const [formValue, setformValue] = useState({id: '', name: '', price: ''});
+  const [isError, setError] = useState(false);
+
+  const onFormSubmit = (evt: React.SyntheticEvent) => {
+    evt.preventDefault();
+    if (!formValue.id || !formValue.name || !formValue.price) {
+      setError(true);
+      return;
+    }
+    addNewProduct(
+      {
+        id: +formValue.id,
+        name: formValue.name,
+        price: +formValue.price,
+      }
+    );
+  };
+
+  const onInputChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    setError(false);
+    console.log(evt.target.value);
+    setformValue({...formValue, [evt.target.name]: evt.target.value});
+  }
+
   return (
-    <div className="new-product"></div>
+    <div className="new-product">
+      <h2 className="new-product__title">Добавить товар</h2>
+      <form className="new-product__form"  onSubmit={onFormSubmit}>
+        <input className="new-product__input new-product__input--id" type="number" name="id" placeholder="ID" value={formValue.id} onChange={onInputChange}/>
+        <input className="new-product__input new-product__input--name" type="text" name="name" placeholder="Название товара" value={formValue.name} onChange={onInputChange}/>
+        <input className="new-product__input new-product__input--price" type="number" name="price" placeholder="Цена" value={formValue.price} onChange={onInputChange}/>
+        <button className="new-product__add-button" type="submit">Добавить</button>
+      </form>
+      
+      {isError ? <p className="new-product__error-message">Заполните все поля!</p> : null }
+    </div>
   );
 };
 
